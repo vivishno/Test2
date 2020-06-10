13c26a521ffb9db072eed6d785d7f3c995860aa8
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-10 12:44:56 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -71,8 +71,9 @@ def main():
     service_principal_password=azure_credentials.get("clientSecret", "")
     print("5--------------------------------------")
     
-    command = ('az login --service-principal --username {APP_ID} --password {PASSWORD} --tenant {TENANT_ID}').format(
-            APP_ID=service_principal_id, PASSWORD=service_principal_password, TENANT_ID=tenant_id)
+    #command = ('az login --service-principal --username {APP_ID} --password {PASSWORD} --tenant {TENANT_ID}').format(
+    #       APP_ID=service_principal_id, PASSWORD=service_principal_password, TENANT_ID=tenant_id)
+    command='az login --service-principal --username "ab96606e-49a7-45d3-a575-5172e11fdb7f" --password "^s:e6b4uCMXxN168t+i?[f](`E~8YeAP" --tenant "2d1aba9c-5938-402b-90b9-72a284a4bced"'
     try:
         app_create = subprocess.check_output(command, shell=True)
         print(app_create)
